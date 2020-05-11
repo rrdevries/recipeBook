34933068c3201c8c38a4b326adb24722a653c8d5
Renee de Vries <r.r.devries@uva.nl>
2020-05-11 15:19:06 +0200

    .

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,9 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
+  isOpen = false;
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+
+  }
 
   constructor() { }
 
