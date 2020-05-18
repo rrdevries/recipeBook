7b55666c8927926efa5eeedfd1369da32af1b642
Renee de Vries <r.r.devries@uva.nl>
2020-05-18 13:12:56 +0200

    Implement RecipeService

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,8 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
   
   private recipes: Recipe[] = [
     new Recipe('A test Recipe', 'This is simply a test', 'https://img.taste.com.au/62w0bkp3/w720-h480-cfill-q80/taste/2018/02/mar-18_creamy-fettuccine-with-chicken-3000x2000-135690-1.jpg'),
@@ -10,5 +12,7 @@ export class RecipeService {
   getRecipe() {
     return this.recipes.slice();
   }
+
+
 
 }
