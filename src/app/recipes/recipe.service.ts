92731791c5bfe3abf3185f343342b97e7779ff5a
Renee de Vries <r.r.devries@uva.nl>
2020-05-20 13:32:05 +0200

    Recipe service filled in

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,32 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   
   private recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is simply a test', 'https://img.taste.com.au/62w0bkp3/w720-h480-cfill-q80/taste/2018/02/mar-18_creamy-fettuccine-with-chicken-3000x2000-135690-1.jpg'),
-    new Recipe('A test Recipe', 'This is simply a test', 'https://img.taste.com.au/62w0bkp3/w720-h480-cfill-q80/taste/2018/02/mar-18_creamy-fettuccine-with-chicken-3000x2000-135690-1.jpg')
+    new Recipe(
+      'A tasty schnitzel', 
+      'Schnitzel has never been this good', 
+      'https://img.taste.com.au/62w0bkp3/w720-h480-cfill-q80/taste/2018/02/mar-18_creamy-fettuccine-with-chicken-3000x2000-135690-1.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Lemon', 1),
+        new Ingredient('Egg', 2),
+        new Ingredient('French Fries', 20),
+      ]
+    ),
+    new Recipe(
+      'Chicker Parmezan', 
+      'Juicy chicken with a crispy skin', 
+      'https://img.taste.com.au/62w0bkp3/w720-h480-cfill-q80/taste/2018/02/mar-18_creamy-fettuccine-with-chicken-3000x2000-135690-1.jpg',
+      [
+         new Ingredient('Chicken', 1),
+         new Ingredient('Parmezan cheese', 1),
+          new Ingredient('Milk', 2),
+      ]
+      )
   ];
 
   getRecipe() {
